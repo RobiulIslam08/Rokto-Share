6f6aee92177b51f6dc3a4ae272800414f814873c
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-10-13 12:16:45 +0600

    token add to localstorage

--- src/redux/features/auth/authSlice.ts
+++ src/redux/features/auth/authSlice.ts
@@ -1,10 +1,10 @@
-import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
 type TUser = {
   _id: string;
   name: string;
   email: string;
-  role: 'user' | 'donor' | 'admin';
+  role: "user" | "donor" | "admin";
 };
 
 type TAuthState = {
@@ -18,23 +18,28 @@ const initialState: TAuthState = {
 };
 
 const authSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState,
   reducers: {
     setUser: (state, action: PayloadAction<{ user: TUser; token: string }>) => {
       const { user, token } = action.payload;
       state.user = user;
       state.token = token;
-      
-      localStorage.setItem('token', action.payload.token)
+
+      localStorage.setItem("token", action.payload.token);
+    },
+    setToken: (state, action: PayloadAction<string>) => {
+      state.token = action.payload;
+      localStorage.setItem('token', action.payload)
     },
     logout: (state) => {
       state.user = null;
       state.token = null;
+      localStorage.removeItem('token');
     },
   },
 });
 
-export const { setUser, logout } = authSlice.actions;
+export const { setUser, setToken,logout } = authSlice.actions;
 
 export default authSlice.reducer;

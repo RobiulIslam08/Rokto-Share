86fe3c386226f893d23bde09174e21bf2c60af61
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-10-17 14:57:49 +0600

    updateUserProfile query create

--- src/redux/features/user/userApi.ts
+++ src/redux/features/user/userApi.ts
@@ -10,7 +10,15 @@ export const userApi = baseApi.injectEndpoints({
       providesTags: ['userProfile'],
     }),
     // ✅ NEW: This mutation updates the profile
-  
+    updateUserProfile: builder.mutation({
+      query: (data) => ({
+        url: '/users/profile',
+        method: 'PUT', // Or 'PATCH' depending on your backend controller
+        body: data,
+      }),
+      // This will auto-re-fetch getUserProfile after a successful update
+      invalidatesTags: ['userProfile'], 
+    }),
   }),
 });
 export const {useGetUserProfileQuery, useUpdateUserProfileMutation} = userApi
